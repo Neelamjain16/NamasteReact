51578245d5c3fb891d60414891382f75c0179729
neelamjain16 <neelamrghjain@gmail.com>
2024-04-27 18:02:04 +0530

    first app completed

--- firstapp/App.js
+++ firstapp/App.js
@@ -1,4 +1,8 @@
-const heading=React.createElement('h1',{id:'heading'},'this is heading from react');
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const heading=React.createElement('h1',{id:'heading'},'this is heading from react 🐱‍🚀 nacho nacho ');
 console.log(heading)//this is a react element (it will be an object)
 const root=ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading)
+
